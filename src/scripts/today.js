8570f94243175d6dc30f9199d276bfa145f7a83a
Jørgen Borgesen <joergen.borgesen@gmail.com>
2015-04-22 15:55:52 +0200

    Added jshint config and introduced some more JS problems

--- src/scripts/today.js
+++ src/scripts/today.js
@@ -1,7 +1,12 @@
-var now = new Date();
-var start = new Date(now.getFullYear(), 0, 0);
-var diff = now - start;
-var oneDay = 1000 * 60 * 60 * 24;
-var day = Math.floor(diff / oneDay);
+function getDayOfYear(input){
+	var now = new Date();
+	var start = new Date(now.getFullYear(), 0, 0);
+	var diff = now - start;
+	var oneDay = 1000 * 60 * 60 * 24;
+	var unusedVar = 10 * 10;
 
+	return Math.floor(diff / oneDay);
+}
+
+var day = getDayOfYear();
 console.log("Idag er den " + day + '. dagen i året.');
